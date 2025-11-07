5321db42aab1a123341de9aa36f42a3763c86077
Aryan Kumar <aryankumar.linux@gamail.com>
2025-11-07 11:35:44 +0530

    Added LoginBtn component with styling and hover effects

---
+++ client/src/components/LoginBtn.jsx
@@ -0,0 +1,11 @@
+
+
+function LoginBtn(){
+    return(
+        <div className="px-5 py-3 bg-[#101828] text-white rounded-md h-[2.3rem] w-[17rem] flex justify-center items-center cursor-pointer shadow-md transition-all duration-300 hover:[transform:translateY(-.200rem)] hover:shadow-xl border-none">
+            <h2 className="font-inter">Login</h2>
+        </div>
+    )
+}
+
+export default LoginBtn
